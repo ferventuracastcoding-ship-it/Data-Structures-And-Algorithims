29b46a50899f8f0680bf00c7bfaa74b71d558b10
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-13 18:40:20 -0700

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -2,3 +2,35 @@ const binarySearch(key, value) {
   console.log("Binary Search");
   return key + value;
 }
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while(left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const current = arr[mid];
+
+    if(current === target) {
+      retrun mid;
+    }
+    if(current < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+const sortedNumbers = [2,5,8,12,16,20,38,56,72,91];
+console.log(binarySearch(sortedNumbers, 23));
+console.log(binarySearch(sortedNumbers, 10));
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length -1;
+
+  while(left <= right) {
+    // code
+  }
+}
