921741d8e3ff277b17439b37a6bbb61e5c72136e
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-09-08 22:50:08 -0700

    Implement initial mergeSort function structure

---
+++ MergeSort.js
@@ -0,0 +1,11 @@
+function mergeSort(arr) {
+  if (arr.length >= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  
+function merge(left,right) {
+  const result = [];
+  let j = 0;
+  let i = 0;
+}
+  slice.right((j));
+}
